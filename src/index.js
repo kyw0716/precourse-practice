a07ce95d8612bc2edbc0fab303e8822fdc137904
kyw0716 <kyw0716@naver.com>
2022-10-25 15:33:31 +0900

    feat: play 매서드 추가
    입력값, 컴퓨터 생성 값을 비교해 힌트를 생성하는 기능

--- src/index.js
+++ src/index.js
@@ -11,7 +11,26 @@ export default function BaseballGame() {
       ...this.state,
       ...nextState,
     };
-    console.log(this.state);
+  };
+
+  this.play = (computerNumber, userNumber) => {
+    const computerNumberArray = String(computerNumber).split("");
+    const userNumberArray = String(userNumber).split("");
+    let strikeCount = 0;
+    let ballCount = 0;
+    let hint = "";
+
+    userNumberArray.forEach((v, i) => {
+      if (v === computerNumberArray[i]) strikeCount += 1;
+      if (computerNumberArray.includes(v)) ballCount += 1;
+    });
+    ballCount -= strikeCount;
+
+    if (ballCount > 0) hint += `${ballCount}볼 `;
+    if (strikeCount > 0) hint += `${strikeCount}스트라이크`;
+    if (hint.length === 0) hint += `낫싱`;
+
+    return hint;
   };
 
   const UserInputComponent = new UserInput({
